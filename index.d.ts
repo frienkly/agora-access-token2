f29ca4d10ae3c32f969ce44a8714eef14e0ef85c
유근국 <yugeungook@yugeungug-ui-MacBookPro.local>
2022-09-28 22:13:45 +0900

    fix typo

--- index.d.ts
+++ index.d.ts
@@ -21,6 +21,7 @@ export namespace RtcTokenBuilder {
      * @param {*} role See #userRole.
      * - Role.PUBLISHER; RECOMMENDED. Use this role for a voice/video call or a live broadcast.
      * - Role.SUBSCRIBER: ONLY use this role if your live-broadcast scenario requires authentication for [Hosting-in](https://docs.agora.io/en/Agora%20Platform/terms?platform=All%20Platforms#hosting-in). In order for this role to take effect, please contact our support team to enable authentication for Hosting-in for you. Otherwise, Role_Subscriber still has the same privileges as Role_Publisher.
+     * - Role.AUDIO_ONLY_PUBLISHER; Use this role for a voice call or a live broadcast.
      * @param {*} token_expire epresented by the number of seconds elapsed since now. If, for example, you want to access the Agora Service within 10 minutes after the token is generated, set token_expire as 600(seconds)
      * @param {*} privilege_expire represented by the number of seconds elapsed since now. If, for example, you want to enable your privilege for 10 minutes, set privilege_expire as 600(seconds).     * @return The new Token.
      */
@@ -40,11 +41,12 @@ export namespace RtcTokenBuilder {
      * @param {*} role See #userRole.
      * - Role.PUBLISHER; RECOMMENDED. Use this role for a voice/video call or a live broadcast.
      * - Role.SUBSCRIBER: ONLY use this role if your live-broadcast scenario requires authentication for [Hosting-in](https://docs.agora.io/en/Agora%20Platform/terms?platform=All%20Platforms#hosting-in). In order for this role to take effect, please contact our support team to enable authentication for Hosting-in for you. Otherwise, Role_Subscriber still has the same privileges as Role_Publisher.
+     * - Role.AUDIO_ONLY_PUBLISHER; Use this role for a voice call or a live broadcast.
      * @param {*} token_expire epresented by the number of seconds elapsed since now. If, for example, you want to access the Agora Service within 10 minutes after the token is generated, set token_expire as 600(seconds)
      * @param {*} privilege_expire represented by the number of seconds elapsed since now. If, for example, you want to enable your privilege for 10 minutes, set privilege_expire as 600(seconds).
      * @return The new Token.
      */
-    export function buildTokenWithAccount(appID: string, appCertificate: string, channelName: string, account: string, role: number, token_expire:number, privilege_expire: number): string;
+    export function buildTokenWithUserAccount(appID: string, appCertificate: string, channelName: string, account: string, role: number, token_expire:number, privilege_expire: number): string;
 }
 
 export namespace RtmTokenBuilder {
